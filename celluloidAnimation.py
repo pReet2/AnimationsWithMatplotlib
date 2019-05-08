209ac1f3f1dcffbedecd59042b789dea0330f458
pReet2 <preetiitzme@gmail.com>
2019-05-09 01:05:40 +0530

    Add files via upload
    Create animation with Celluloid which is a Python Module. An animation is created with all the captured frame

---
+++ celluloidAnimation.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt 
+
+from celluloid import Camera
+fig = plt.figure()
+camera = Camera(fig)
+for i in range(10):
+
+    plt.plot([i]*10)
+    camera.snap()
+
+animation = camera.animate()
+plt.show()
